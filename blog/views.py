cedb95272ac10987190bf6b3fc15391572e9f020
Your Name <you@example.com>
2021-02-26 14:52:50 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -10,5 +10,5 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
-    post = get_list_or_404(Post, pk=pk)
+    post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
